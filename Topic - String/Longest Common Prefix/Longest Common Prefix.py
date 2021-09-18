bfe62beddcba6a1f27128175ac76d6ebaa13b6d6
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-09-18 20:54:48 +0530

    Python Code for Longest Common Prefix.

--- Topic - String/Longest Common Prefix/Longest Common Prefix.py
+++ Topic - String/Longest Common Prefix/Longest Common Prefix.py
@@ -1,13 +1,3 @@
-#Longest Common Prefix.
-#Arr = ["aa" , "aab" , "aabvde"]
-"""
-Problem Description >>
-
-Given the array of strings A, you need to find the longest string S which is the prefix of ALL the strings in the array.
-Longest common prefix for a pair of strings S1 and S2 is the longest string S which is the prefix of both S1 and S2.
-
-For Example: longest common prefix of "abcdefgh" and "abcefgh" is "abc".
-"""
 #Solution : 1
 
 def longestcommonprefix(Arr):
