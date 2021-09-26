0d08b0715b34010f0487ba4bf2c59c121bcfb610
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-09-26 16:13:37 +0530

    Merge overlapping intervals

--- Topic - Array/Merge Intervals.py
+++ Topic - Array/Merge Intervals.py
@@ -19,3 +19,17 @@ def mergeintervals(Arr):
     return mergedintervals
 
 print(mergeintervals([[2,4] , [4,7] , [6,8] , [1,9] ]))
+
+
+#Another approach
+
+def merge(self, intervals):
+    intervals.sort(key = lambda i:i.start)
+    merged = []
+    merged.append(intervals[0])
+    for j in range(1 , len(intervals)):
+        if intervals[j].start > merged[-1].end:
+            merged.append(intervals[j])
+        else:
+            merged[-1].end = max(intervals[j].end , merged[-1].end)
+    return merged
