30f32a73e2acc827ee4f1e627eb1de6a47e21652
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-09-28 19:35:03 +0530

    Topic : Array

--- Topic - Array/Merge Non Overlapping Intervals.py
+++ Topic - Array/Merge Non Overlapping Intervals.py
@@ -1,3 +1,15 @@
+"""
+Problem Statement :
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9] insert and merge [2,5] would result in [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] would result in [1,2],[3,10],[12,16].
+"""
+
 def insert(self, intervals, new_interval):
         intervals.append(new_interval)
         intervals.sort(key = lambda i:i.start)
@@ -9,3 +21,5 @@ def insert(self, intervals, new_interval):
             else:
                 merged[-1].end = max(intervals[j].end , merged[-1].end)
         return merged
+
+#TC : O(nlogn)
