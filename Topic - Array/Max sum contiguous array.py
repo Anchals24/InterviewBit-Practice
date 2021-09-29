2d7fbbd1809b038c89d82de64af394b4e029ab36
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-09-29 10:56:53 +0530

    Topic : Array

--- Topic - Array/Max sum contiguous array.py
+++ Topic - Array/Max sum contiguous array.py
@@ -1,4 +1,17 @@
-#Solution 1 : TLE :(
+"""
+PROBLEM STATEMENT: 
+Find the contiguous subarray within an array, A of length N which has the largest sum.
+
+Input Format:
+
+The first and the only argument contains an integer array, A.
+Output Format:
+
+Return an integer representing the maximum possible sum of the contiguous subarray.
+"""
+
+#Solution 1 : TLE :( 
+#Complexity : O(n**2)
 
 import math
 A = [-163 , -23]
@@ -13,7 +26,8 @@ for a in range(l):
         maxi = max(maxi , summ)
 print(maxi)
 
-#Solution 2 : Greedy Approach [Optimized] :D
+#Solution 2 : Greedy Approach [Optimized] :D 
+#Kadane's Algorithm TC : O(n)
 
 Arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 current_sum = 0
